8c06eaa0c39f3961939910d7dec5cc154881fe4d
zandybeats <alexandrumateescu02@gmail.com>
2022-01-05 14:32:49 +0200

    Initial commit

---
+++ ServerNavigator_common/src/main/java/com/github/zandy/servernavigator/Main.java
@@ -0,0 +1,16 @@
+package com.github.zandy.servernavigator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
